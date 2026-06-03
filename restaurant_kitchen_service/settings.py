6d86a9ee5922655201e6c4c494c48f3e4340d8ee
Shongi <shongi85@gmail.com>
2026-06-03 19:43:22 +0200

    Added models and migrations

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Custom user model
+# https://docs.djangoproject.com/en/6.0/topics/auth/customizing/#substituting-a-custom-user-model
+
+AUTH_USER_MODEL = "kitchen.Cook"
